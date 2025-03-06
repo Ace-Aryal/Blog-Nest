f6d4d6d23d452136d2d5c9433cc04f939a91d712
Ace-Aryal <aryaldeelep@gmail.com>
2025-03-06 20:35:07 +0545

    Initial setup for appwrite an reach

---
+++ Blog-Nest/src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+
+import './App.css'
+
+function App() {
+console.log(import.meta.env.VITE_APPWRITE_URL);
+
+  return (
+   <>Hello World</>
+  )
+}
+
+export default App
---
+++ Blog-Nest/src/config/config.js
@@ -0,0 +1,13 @@
+const config = {
+    appwritreURL : String(import.meta.env.VITE_APPWRITE_URL),
+    appwritreProjectID : String(import.meta.env.VITE_APPWRITE_PROJECT_ID) ,
+    appwritreDatabaseID : String(import.meta.env.VITE_APPWRITE_DATABASAE_ID),
+    appwritreCollectionID : String(import.meta.env.VITE_APPWRITE_COLLECTION_ID),
+    appwritreBucketID : String(import.meta.env.VITE_APPWRITE_BUCKET_ID) 
+
+
+
+
+}
+
+export default config
