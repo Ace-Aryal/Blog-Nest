774a36e4bdda321ba8904c0fb34481ad31112f36
Ace-Aryal <aryaldeelep@gmail.com>
2025-03-07 20:28:43 +0545

    Appwrite Set Up

---
+++ Blog-Nest/src/appwrite/auth.js
@@ -0,0 +1,72 @@
+// Auth for Our App
+
+// this appwrite auth can be used for any project just copy and paste but read docs for latest services 
+
+import config from "../config/config";
+
+import { Client, Account, ID } from "appwrite"; // ID gives unique id
+
+export class AuthService {
+  client = new Client();
+  account;
+  constructor() {
+    this.client
+      .setEndpoint(config.appwritreURL)
+      .setEndpoint(config.appwritreProjectID);
+    this.account = new Account(this.client);
+  }
+
+  async createAccount({ email, password, username }) {
+    try {
+      const userAccount = await this.createAccount(
+        ID.unique(),
+        email,
+        password,
+        username
+      ); // this parameter structure is defined in the docmentation
+
+      if (!userAccount) {
+        
+
+        return userAccount
+      }
+        return this.login({email , password})
+      // if the account is created log them in automatically 
+      
+    
+    } catch (error) {
+      throw error;
+    }
+  }
+
+  async login ({email ,password , username} ){
+    try {
+        await this.account.createEmailPasswordSession(email, password)
+    } catch (error) {
+        throw error
+    }
+  }
+
+  async getCurrentUser() {
+    try {
+
+       return await this.account.get()
+    } catch (error) {
+        throw error
+    }
+
+    return null
+  }
+
+  async logout () {
+    try {
+        await this.account.deleteSessions()
+    } catch (error) {
+        throw error
+    }
+  }
+}
+
+const authService = new AuthService();
+
+export default authService;
---
+++ Blog-Nest/src/appwrite/conf.js
@@ -0,0 +1,129 @@
+// for appwrite database , uploading files and custom queries
+
+import config from "../config/config";
+import { Client, ID, Databases, Query, Storage } from "appwrite";
+
+export class services {
+  client = new Client();
+  databases;
+  storage; // bucket
+
+  constructor() {
+    this.client
+      .setEndpoint(config.appwritreURL)
+      .setEndpoint(config.appwritreProjectID);
+    this.storage = new Storage(this.client);
+    this.databases = new Databases(this.client);
+  }
+
+  // using slug as id
+  async createPost({ title, slug, content, featuredImage, status, userID }) {
+    try {
+      return this.databases.createDocument(
+        config.appwritreDatabaseID,
+        config.appwritreCollectionID,
+        {
+          title,
+          content,
+          featuredImage,
+          status,
+          userID,
+        }
+      );
+    } catch (error) {
+      throw error;
+    }
+  }
+
+  async updatePost(slug, { title, content, featuredImage, status, userID }) {
+    try {
+      return await this.databases.updateDocument(
+        config.appwritreDatabaseID,
+        config.appwritreCollectionID,
+        slug,
+        {
+          title,
+          content,
+          featuredImage,
+          status,
+
+        }
+      )
+    } catch (error) {
+      throw error
+    }
+  }
+
+  async deletePost(slug) {
+    try {
+      await this.databases.deleteDocument(
+        config.appwritreDatabaseID,
+        config.appwritreCollectionID,
+        slug
+      )
+      return true
+    } catch (error) {
+      throw error
+    }
+  }
+
+  async getPost(slug) {
+    try {
+      return await this.databases.getDocument(
+        config.appwritreDatabaseID,
+        config.appwritreCollectionID,
+        slug
+      )
+    } catch (error) {
+      throw error
+    }
+
+  }
+
+  async getPosts(queries = Query.equal("status", "active")) {
+
+    try {
+      return this.databases.listDocuments(
+        config.appwritreDatabaseID,
+        config.appwritreCollectionID,
+        queries // note : queries can also be written here in  array
+      )
+    } catch (error) {
+      throw error
+    }
+  }
+
+  // file upload service 
+
+  async uploadFile(file) {
+    try {
+      return await this.storage.createFile(
+        config.appwritreBucketID,
+        ID.unique(),
+        file
+      )
+    } catch (error) {
+      throw error
+
+    }
+  }
+
+  async deleteFile(fileID) {
+    try {
+      await this.deleteFile(
+        config.appwritreBucketID,
+        fileID
+      )
+    } catch (error) {
+      throw error
+    }
+  }
+
+  getFilePreview(fileID) {
+    return this.storage.getFilePreview(
+      config.appwritreBucketID,
+      fileID
+    )
+  }
+
+}
