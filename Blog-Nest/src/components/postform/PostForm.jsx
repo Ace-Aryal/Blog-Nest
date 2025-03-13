b90ab6293840fbdac1c90702c832037b2bed5659
Ace-Aryal <aryaldeelep@gmail.com>
2025-03-13 22:48:01 +0545

    worked on useForm and revised appwrite

--- Blog-Nest/src/components/postform/PostForm.jsx
+++ Blog-Nest/src/components/postform/PostForm.jsx
@@ -4,7 +4,29 @@ import { Input, Select, RTE } from "../index";
 import appwriteService from "../../appwrite/conf";
 import { useNavigate } from "react-router-dom";
 import { useSelector } from "react-redux";
-function postform() {
+function postform({ post }) {
+  const { register, handleSubmit, watch, setValue, control, getValues } =
+    useForm({
+      defalutValues: {
+        title: post?.title || "",
+        slug: post?.slug || "",
+        content: post?.content || "",
+        status: post?.status || "active",
+      },
+    });
+  const navigate = useNavigate();
+  const userData = useSelector((state) => state.user.userData);
+
+  const sumbit = (data) => {
+    if (post) {
+      const file = data.image[0]
+        ? appwriteService.uploadFile(data.image[0])
+        : null;
+    }
+    if (file) {
+      appwriteService.deleteFile(post.featuredImage);
+    }
+  };
   return <div>postform</div>;
 }
 
