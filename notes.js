bdab4c7435bb28a2120316238be850a6c8a6c4f2
Dipesh Aryal <153974691+Ace-Aryal@users.noreply.github.com>
2025-03-31 09:44:34 +0545

    Update notes.js

--- notes.js
+++ notes.js
@@ -1,2 +1,12 @@
-// Steps I followed
-//1. installed all dependencies and packages i needed 
+What to learn from this project ?
+1. Using Appwrite and correct method to handle appwrite creating object ; can also be used when designing other services
+2. Linking Database API and Storage API
+3. Using React hook form (validation , easy handling , easy tracking : Watch and Controller )
+4. RTE using react hook form and html react parser 
+5. Protected Route <Authlayout>
+6.Production grade components and making componentd for every thing so that it is modular and each file is small in size (Single responsibility principle)
+7. Authentication and authorization (reading vs writing )
+8. State management so that we hit web request only when needed and handle other things through state
+9.handle Loading when fetching data , submitting forms , creaating new entries
+10.handle server errors for auth and others requests sent, so that state and database and ui is in sync 
+
