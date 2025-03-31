3f2102e5ae42cf279e9f97649c7a9fdbd47ea46a
Dipesh Aryal <153974691+Ace-Aryal@users.noreply.github.com>
2025-03-31 11:17:22 +0545

    Update notes.js

--- notes.js
+++ notes.js
@@ -4,7 +4,8 @@ What to learn from this project ?
 3. Using React hook form (validation , easy handling , easy tracking : Watch and Controller )
 4. RTE using react hook form and html react parser 
 5. Protected Route <Authlayout>
-6.Production grade components and making componentd for every thing so that it is modular and each file is small in size (Single responsibility principle)
+6.Production grade components and making componentd for every thing so that it is modular and each file is small in size (Single responsibility principle) 
+and prevent big remount of component
 7. Authentication and authorization (reading vs writing )
 8. State management so that we hit web request only when needed and handle other things through state
 9.handle Loading when fetching data , submitting forms , creaating new entries
